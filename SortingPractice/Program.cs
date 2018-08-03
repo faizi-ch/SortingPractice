f8b381d340fd100e69a168b73ee3eaee53773e57
faizi-ch <faizi.ch@live.com>
2018-08-03 21:09:46 +0500

    Add project files.

---
+++ SortingPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] c = {'a', '(', 'L', 't', ',', 'R', '^', '`', '|', 'Y', '}', 'u', '+', 'q', '=', 'F', '!'};
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                for (int j = 1; j < c.Length; j++)
+                {
+                    if (c[j]>=65 && c[j]<=90 && c[i] >= 65 && c[i] <= 90)
+                    {
+                        char ch = c[j];
+                        c[j] = c[i];
+                        c[i] = ch;
+                    }
+                   /* if (c[j]<c[i])
+                    {
+                        char ch = c[j];
+                        c[j] = c[i];
+                        c[i] = ch;
+                    }*/
+                }
+            }
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                Console.Write(c[i]);
+            }
+
+            Console.ReadKey(true);
+            
+        }
+    }
+}
